f688202fbdf86f17da9f343c3131dca773ce5773
Erkan özgün <eozgun65@gmail.com>
2025-05-07 00:02:56 +0300

    Frontend eklendi

---
+++ Frontends/ETrade.WebUI/ViewComponents/UILayoutViewComponents/_HeadUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETrade.WebUI.ViewComponents.UILayoutViewComponent
+{
+    public class _HeadUILayoutComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
