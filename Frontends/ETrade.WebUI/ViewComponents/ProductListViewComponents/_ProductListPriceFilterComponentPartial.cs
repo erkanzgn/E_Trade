f5988c5ddd71471d6ab8d1a9689f31569015a61e
Erkan özgün <eozgun65@gmail.com>
2025-05-07 10:45:49 +0300

    productList ve productdetail için önyüz tasarımları yapıldı

---
+++ Frontends/ETrade.WebUI/ViewComponents/ProductListViewComponents/_ProductListPriceFilterComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ETrade.WebUI.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListPriceFilterComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
